0dceaf873f56eb20a5d6b61509e9743e96d8e39c
richard <richard>
2019-10-21 21:22:13 -0700

    no-std

--- examples/helloworld/src/lib.rs
+++ examples/helloworld/src/lib.rs
@@ -1,10 +1,8 @@
-#![no_std]
 #![no_main]
+#![no_std]
+#![feature(core_intrinsics, lang_items, alloc_error_handler)]
 
 use js_ffi::*;
-
-#[global_allocator]
-static ALLOCATOR: malloc::Allocator = malloc::Allocator;
 
 #[no_mangle]
 pub fn main() -> () {
@@ -12,4 +10,17 @@ pub fn main() -> () {
     let log = register("console.log");
     // call it with no context and a string
     call_1(UNDEFINED, log, TYPE_STRING, to_js_string("Hello World"));
+}
+
+#[global_allocator]
+static ALLOCATOR:malloc::Allocator = malloc::Allocator;
+
+#[panic_handler]
+fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[alloc_error_handler]
+fn foo(_: core::alloc::Layout) -> ! {
+    loop {}
 }
