400466c08b1b3ce815d37307790f4bd7a992e262
richard <richard>
2019-11-07 17:38:22 -0800

    array support

--- src/lib.rs
+++ src/lib.rs
@@ -170,6 +170,16 @@ pub const TYPE_BOOL: JSType = 3;
 pub const TYPE_FUNCTION: JSType = 4;
 pub const TYPE_OBJECT: JSType = 5;
 pub const TYPE_UINT8_ARRAY: JSType = 6;
+pub const TYPE_INT8_ARRAY: JSType = 7;
+pub const TYPE_UINT8CLAMPED_ARRAY: JSType = 8;
+pub const TYPE_INT16_ARRAY: JSType = 9;
+pub const TYPE_UINT16_ARRAY: JSType = 10;
+pub const TYPE_INT32_ARRAY: JSType = 11;
+pub const TYPE_UINT32_ARRAY: JSType = 12;
+pub const TYPE_F32_ARRAY: JSType = 13;
+pub const TYPE_F64_ARRAY: JSType = 14;
+pub const TYPE_BI64_ARRAY: JSType = 15;
+pub const TYPE_BUI64_ARRAY: JSType = 16;
 
 pub fn release(obj: JSValue) {
     unsafe { jsffirelease(obj) }
@@ -403,6 +413,21 @@ pub fn to_js_typed_array<T>(s: &Vec<T>) -> TypedArray {
     TypedArray {
         length: s.len() as u32,
         pointer: s.as_ptr() as u32,
+    }
+}
+
+pub fn to_typed_array<T>(ptr:JSValue) -> Vec<T> where T:Copy {
+    unsafe {
+        let start = ptr as usize;
+        let lptr = start as *const usize;
+        let length = *lptr;
+        let mut v = Vec::with_capacity(length);
+        let mut data_ptr = ptr as usize+core::mem::size_of::<usize>();
+        for _ in 0..length {
+            v.push(*(data_ptr as *const T));
+            data_ptr += core::mem::size_of::<T>();
+        }
+        v
     }
 }
 
