16ae6be12f38e60960666c9f84da0320e26869f2
Richard Anaya <richard.anaya@gmail.com>
2019-11-09 22:07:05 -0800

    Create js_ffi.h

---
+++ js_ffi.h
@@ -0,0 +1,7 @@
+#define export __attribute__((visibility("default")))
+typedef double JSValue;
+extern int jsffiregister(char*);
+extern void jsfficall1(JSValue,int,int,JSValue);
+
+JSValue const UNDEFINED = 0.0;
+int const TYPE_STRING = 2;
