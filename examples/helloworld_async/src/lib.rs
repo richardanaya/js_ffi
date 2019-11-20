67eacbe32d13766d0e53be088887d562752bc59c
Richard <Richard>
2019-11-19 17:48:04 -0800

    cleanup

--- examples/helloworld_async/src/lib.rs
+++ examples/helloworld_async/src/lib.rs
@@ -1,19 +1,18 @@
 use js_ffi::*;
 
-async fn run() {
-    let console_log = js!(console.log);
-    let set_timeout = js!(window.setTimeout);
-
-    console_log.invoke_1("Hello");
-
-    let (future, cb) = create_callback_future_0();
-    set_timeout.invoke_2(cb, 1000);
-    future.await;
-
-    console_log.invoke_1("world!");
-}
-
 #[no_mangle]
 pub fn main() -> () {
-    executor::spawn(run());
+    executor::spawn(async {
+        let console_log = js!(console.log);
+        console_log.invoke_1("Hello");
+        sleep(1000).await;
+        console_log.invoke_1("world!");
+    });
+}
+
+fn sleep(millis: u32) -> impl core::future::Future {
+    let set_timeout = js!(window.setTimeout);
+    let (future, cb) = create_callback_future_0();
+    set_timeout.invoke_2(cb, millis);
+    future
 }
