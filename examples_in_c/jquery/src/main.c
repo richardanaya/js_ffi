027e5b52c091bf2593cfc24fe9d290555fb2a1b4
Richard <Richard>
2020-03-15 10:11:08 -0700

    adding jquery example

---
+++ examples_in_c/jquery/src/main.c
@@ -0,0 +1,9 @@
+ 
+#define export __attribute__((visibility("default")))
+#include "../../js_ffi.h"
+
+export int main() {
+	int log = jsffiregister("function(msg) { $(\"body\").html(msg); }");
+	jsfficall1(JS_UNDEFINED,log,TYPE_STRING,(JSValue)(int)&"Hello World!");
+	return 0;
+}
