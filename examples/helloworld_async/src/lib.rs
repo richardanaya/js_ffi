a93bb9ca57e84c072169162b510671d80e8d4b77
Richard <Richard>
2020-03-22 13:46:58 -0700

    update

--- examples/helloworld_async/src/lib.rs
+++ examples/helloworld_async/src/lib.rs
@@ -3,6 +3,25 @@
 
 use js_ffi::*;
 
+#[no_mangle]
+pub fn main() -> () {
+    executor::spawn(async {
+        let console_log = js!(console.log);
+        console_log.invoke_1("Hello");
+        sleep(1000).await;
+        console_log.invoke_1("world!");
+    });
+}
+
+fn sleep(millis: u32) -> impl core::future::Future {
+    let set_timeout = js!(window.setTimeout);
+    let (future, cb) = create_callback_future_0();
+    set_timeout.invoke_2(cb, millis);
+    future
+}
+
+// memory allocator stuff
+
 extern crate wee_alloc;
 
 // Use `wee_alloc` as the global allocator.
@@ -19,20 +38,4 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 fn oom(_: core::alloc::Layout) -> ! {
     throw_error("Ran out of memory");
     loop {}
-}
-#[no_mangle]
-pub fn main() -> () {
-    executor::spawn(async {
-        let console_log = js!(console.log);
-        console_log.invoke_1("Hello");
-        sleep(1000).await;
-        console_log.invoke_1("world!");
-    });
-}
-
-fn sleep(millis: u32) -> impl core::future::Future {
-    let set_timeout = js!(window.setTimeout);
-    let (future, cb) = create_callback_future_0();
-    set_timeout.invoke_2(cb, millis);
-    future
 }
